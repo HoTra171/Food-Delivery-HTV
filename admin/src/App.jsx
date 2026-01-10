30d0bd754d1b9ef9211d407155cdff23b0e0f334
DELL-TRA\trabn <trabn1712003@gmail.com>
2026-01-10 12:11:19 +0700

    Implement Admin Login, fix 401 error, fix CORS, fix UI glitch

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
 import { Route, Routes } from 'react-router-dom'
@@ -6,6 +6,7 @@ import Orders from './pages/Orders/Orders'
 import List from './pages/List/List'
 import Add from './pages/Add/Add'
 import RevenueReport from './pages/RevenueReport/RevenueReport'
+import Login from './pages/Login/Login'
 import { ToastContainer } from 'react-toastify';
 
 
@@ -13,20 +14,31 @@ const App = () => {
 
   const url = import.meta.env.VITE_API_URL || 'http://localhost:4000'
 
+  const [token, setToken] = React.useState(localStorage.getItem("token") || "");
+
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token])
+
   return (
     <div>
       <ToastContainer />
-      <Navbar />
-      <hr />
-      <div className="app-content">
-        <Sidebar />
-        <Routes>
-          <Route path="/add" element={<Add url={url} />} />
-          <Route path="/list" element={<List url={url} />} />
-          <Route path="/orders" element={<Orders url={url} />} />
-          <Route path="/reports" element={<RevenueReport url={url} />} />
-        </Routes>
-      </div>
+      {token === ""
+        ? <Login url={url} setToken={setToken} />
+        : <>
+          <Navbar />
+          <hr />
+          <div className="app-content">
+            <Sidebar />
+            <Routes>
+              <Route path="/add" element={<Add url={url} />} />
+              <Route path="/list" element={<List url={url} />} />
+              <Route path="/orders" element={<Orders url={url} />} />
+              <Route path="/reports" element={<RevenueReport url={url} />} />
+            </Routes>
+          </div>
+        </>
+      }
     </div>
   )
 }
